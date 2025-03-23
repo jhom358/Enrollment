fe31b9ec2ac5823b589dc2aba7cd74c778502c13
jhom358 <jhomarigan023@gmail.com>
2025-03-24 01:18:52 +0800

    Add method a for faculty

--- Program.cs
+++ Program.cs
@@ -9,8 +9,9 @@ class Enrollment
         Console.WriteLine("ENROLLMENT FOR SCHOOL YEAR 2025-2026\n");
 
         string[] actions = new string[] {
-        "[1] Register as a New Student",
-        "[2] Exit" };
+        "[1] Faculty",
+        "[2] Student",
+        "[3] Exit" };
 
         foreach (string action in actions)
         {
@@ -23,60 +24,96 @@ class Enrollment
         switch (choice)
         {
             case "1":
-                RegisterNewStudent();
+                Faculty();
                 break;
             case "2":
-                Console.WriteLine("Exiting");
+                RegisterNewStudent();
+                break;
+            case "3":
+                Console.WriteLine("\nExiting the system");
                 break;
             default:
-                Console.WriteLine("Invalid Choice. Please choose between 1-2 only. ");
+                Console.WriteLine("Invalid Choice. Please choose between 1-3 only.");
                 break;
         }
-        static void RegisterNewStudent()
+    }
+
+    static void RegisterNewStudent()
+    {
+        Console.WriteLine("\nRegister as a New Student\n");
+
+        Console.Write("Enter your Full Name: ");
+
+        string name = Console.ReadLine();
+
+        Console.WriteLine("\nYour name is: " + name);
+
+        Console.WriteLine("\nChoose your Course you want to apply:\n");
+        string[] courses = { "[1] Bachelor of Science in Information Technology", "[2] Diploma in Information Technology", "[3] Exit" };
+
+        foreach (string course in courses)
         {
-            Console.WriteLine("\nRegister as a New Student\n");
-            Console.Write("Enter your Full Name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("\nYour name is: " + name);
-
-            Console.WriteLine("\nChoose your Course you want to apply:");
-            string[] courses = { "[1] Bachelor of Science in Information Technology", "[2] Diploma in Information Technology", "[3] Exit" };
-
-            foreach (string course in courses)
-            {
-                Console.WriteLine(course);
-            }
-            Console.WriteLine("\nEnter your Course: ");
-            string courseChoice = Console.ReadLine();
-
-            if (courseChoice == "3")
-            {
-                Console.WriteLine("Exiting Enrollment, Thank you for choosing our school!!!");
-                return;
-            }
-            if (courseChoice == "1")
-            {
-                Console.WriteLine("----------------------------------------------");
-                Console.WriteLine("You have chosen Bachelor of Science in Information Technology");
-                Console.ReadKey();
-                Console.WriteLine("You are now registered as a new student of BSIT!");
-            }
-            else if (courseChoice == "2")
-            {
-                Console.WriteLine("----------------------------------------------");
-                Console.WriteLine("You have chosen Diploma in Information Technology");
-                Console.ReadKey();
-                Console.WriteLine("You are now registered as a new student of DIT!");
-            }
-            else
-            {
-                Console.WriteLine("Invalid Course. Please put the correct course what show below.");
-                return;
-            }
-
-
-
+            Console.WriteLine(course);
         }
+        Console.Write("\nEnter your Course: ");
+        string courseChoice = Console.ReadLine();
 
+        if (courseChoice == "3")
+        {
+            Console.WriteLine("Exiting Enrollment, Thank you for choosing our school!!!");
+            return;
+        }
+        if (courseChoice == "1")
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("You have chosen Bachelor of Science in Information Technology");
+            Console.ReadKey();
+            Console.WriteLine("You are now enrolled as a new student of BSIT!");
+        }
+        else if (courseChoice == "2")
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("You have chosen Diploma in Information Technology");
+            Console.ReadKey();
+            Console.WriteLine("You are now enrolled as a new student of DIT!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Course. Please put the correct course number as shown below.");
+            return;
+        }
+    }
+
+    static void Faculty()
+    {
+        Console.WriteLine("\nLog in as a Faculty");
+
+        Console.Write("\nEnter your Faculty Account: ");
+
+        string username = Console.ReadLine();
+
+        if (username == "jhom")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Username Successful.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Username, Please try again.");
+        }
+            Console.WriteLine();
+            Console.Write("Enter your Password: ");
+
+        string password = Console.ReadLine();
+
+        if (username == "jhom" && password == "123")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Log in Successful!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Username or Password. Please try again.");
+        }
     }
 }

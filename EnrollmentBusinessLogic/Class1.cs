0d43e7cfac7c0648f9e7416cc4f772d1fa87849a
jhom358 <jhomarigan023@gmail.com>
2025-03-27 09:31:38 +0800

    Add files via upload

---
+++ EnrollmentBusinessLogic/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace EnrollmentBusinessLogic
+{
+    public class EnrollBusinessLogic
+    {
+        public static List<string> enrolledStudents = new List<string>();
+        public static void studentList(string name, string program)
+        {
+            enrolledStudents.Add(name + "\t" + program);
+        }
+        public static string removedStudent(string name)
+        {
+            string student = enrolledStudents.Find(x => x.Contains(name));
+            enrolledStudents.Remove(student);
+            return student;
+        }
+
+    }
+}
+
